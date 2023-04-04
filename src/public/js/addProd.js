32b79bb9ade2e3323f3daa49f160d13fdc7ebacd
Fpiatti98 <franco.piatti698@gmail.com>
2023-04-04 16:16:54 -0300

    Entrega passport+github

---
+++ src/public/js/addProd.js
@@ -0,0 +1,38 @@
+const addProdToCart = (id) => {
+
+    /*
+    fetch('/js/test.js')
+  .then(response => response.text())
+  .then(data => {
+    // Evaluate the function and use it
+    const myFunction = eval(data);
+  })
+  
+  const response = await fetch('/js/test.js');
+  const data = await response.text()
+  const funcion = await eval(data)
+  */
+
+  fetch(`http://localhost:8080/mongodb/api/carts/641501090cd3fa0a61861a7c/product/${id}`, {
+    method: 'POST',
+    mode: "cors",
+  })
+  .then(result => {
+    if(result.status==200){
+      alert("producto agregado");
+    }
+  });
+  
+};
+
+const logOut = () => {
+  fetch(`http://localhost:8080/api/sessions/logout`, {
+    method: 'GET',
+    mode: "cors",
+  })
+  .then(result => {
+    if(result.status==200){
+      window.location.replace('/users/login')
+    }
+  });
+}
